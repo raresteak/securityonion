e946d722ac1c47eec6a4c9c817e79ccb1ded1543
raresteak <raresteak@users.noreply.github.com>
2019-08-24 09:33:06 -0500

    Update so_postinstall.sh

--- so_postinstall.sh
+++ so_postinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Security Onion Post installation script
 sudo soup
 sudo apt-get install byobu tree docker-compose
+# Installs Suricata and Snort IDS rule and pcap testing system Dalton
+# Nice to have if you are writing custom snort/suricata rules and need to test prior to deploying inside Security Onion
 git clone https://github.com/secureworks/dalton
-sudo dalton/start-dalton.sh
+sudo dalton/start-dalton.sh # takes quite a while.
+sudo so-setup 
